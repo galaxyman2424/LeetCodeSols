d169018d74fa427b06e7deaf4c344d39d25be26e
galaxyman2424 <cwthelen@outlook.com>
2026-06-19 18:48:55 -0400

    added a couple sols

--- 11ContainerWithMostWater.py
+++ 11ContainerWithMostWater.py
@@ -1,6 +1,25 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         maxvol = 0
+        
+        i = 0
+        j = len(height) - 1
+
+        while i!=j:
+            if min(height[i], height[j])*(j-i) > maxvol:
+                maxvol = min(height[i], height[j])*(j-i)
+
+
+            if height[i] > height[j]:
+                j -= 1
+            else:
+                i += 1
+
+        return maxvol
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxvol = 0
         lenlist = len(height)
 
         for i,num in enumerate(height):
@@ -12,4 +31,4 @@ class Solution:
                 if (j-i)*min(height[i], height[j]) > maxvol:
                     maxvol = (j-i)*min(height[i], height[j])
 
-        return maxvol
+        return max
